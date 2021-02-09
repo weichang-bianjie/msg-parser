1752f22cf62c20a06cb7ef054d0ad7751570ee3f
huangweichang <weichang@bianjie.ai>
2021-02-09 10:48:54 +0800

    add iritamod types var

--- types/codec.go
+++ types/codec.go
@@ -1,5 +1,12 @@
 package types
 
-import "github.com/cosmos/cosmos-sdk/simapp/params"
+import (
+	"github.com/bianjieai/iritamod/modules/identity/types"
+	"github.com/cosmos/cosmos-sdk/simapp/params"
+)
 
 type EncodingConfig params.EncodingConfig
+
+var (
+	PubKeyAlgorithm_name = types.PubKeyAlgorithm_name
+)
