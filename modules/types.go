fd212f6297454ed00df833ca555ced69c595b86d
huangweichang <weichang@bianjie.ai>
2021-04-22 13:58:45 +0800

    remove ibc code

--- modules/types.go
+++ modules/types.go
@@ -77,32 +77,6 @@ const (
 	MsgTypeSubmitEvidence  = "submit_evidence"
 	MsgTypeVerifyInvariant = "verify_invariant"
 
-	//ibc client
-	MsgTypeCreateClient             = "create_client"
-	MsgTypeUpdateClient             = "update_client"
-	MsgTypeUpgradeClient            = "upgrade_client"
-	MsgTypeSubmitMisbehaviourClient = "submit_misbehaviour"
-
-	//ibc connect
-	MsgTypeConnectionOpenInit    = "connection_open_init"
-	MsgTypeConnectionOpenTry     = "connection_open_try"
-	MsgTypeConnectionOpenAck     = "connection_open_ack"
-	MsgTypeConnectionOpenConfirm = "connection_open_confirm"
-
-	//ibc channel
-	MsgTypeChannelOpenInit     = "channel_open_init"
-	MsgTypeChannelOpenTry      = "channel_open_try"
-	MsgTypeChannelOpenAck      = "channel_open_ack"
-	MsgTypeChannelOpenConfirm  = "channel_open_confirm"
-	MsgTypeChannelCloseInit    = "channel_close_init"
-	MsgTypeChannelCloseConfirm = "channel_close_confirm"
-	MsgTypeTimeout             = "timeout_packet"
-	MsgTypeTimeoutOnClose      = "timeout_on_close_packet"
-	MsgTypeAcknowledgement     = "acknowledge_packet"
-
-	MsgTypeRecvPacket  = "recv_packet"
-	MsgTypeIBCTransfer = "transfer"
-
 	TxTypeRequestRand = "request_rand"
 
 	TxTypeCreateFeed = "create_feed"
@@ -195,30 +169,6 @@ type (
 
 	MsgSubmitEvidence  = evidence.MsgSubmitEvidence
 	MsgVerifyInvariant = crisis.MsgVerifyInvariant
-
-	//FungibleTokenPacketData = ibctransfer.FungibleTokenPacketData
-	//MsgRecvPacket           = ibc.MsgRecvPacket
-	//MsgTransfer             = ibctransfer.MsgTransfer
-	//
-	//MsgCreateClient       = ibcclient.MsgCreateClient
-	//MsgUpdateClient       = ibcclient.MsgUpdateClient
-	//MsgSubmitMisbehaviour = ibcclient.MsgSubmitMisbehaviour
-	//MsgUpgradeClient      = ibcclient.MsgUpgradeClient
-	//
-	//MsgConnectionOpenInit    = ibcconnect.MsgConnectionOpenInit
-	//MsgConnectionOpenAck     = ibcconnect.MsgConnectionOpenAck
-	//MsgConnectionOpenConfirm = ibcconnect.MsgConnectionOpenConfirm
-	//MsgConnectionOpenTry     = ibcconnect.MsgConnectionOpenTry
-	//
-	//MsgChannelOpenInit     = ibcchannel.MsgChannelOpenInit
-	//MsgChannelOpenTry      = ibcchannel.MsgChannelOpenTry
-	//MsgChannelOpenAck      = ibcchannel.MsgChannelOpenAck
-	//MsgChannelOpenConfirm  = ibcchannel.MsgChannelOpenConfirm
-	//MsgChannelCloseConfirm = ibcchannel.MsgChannelCloseConfirm
-	//MsgChannelCloseInit    = ibcchannel.MsgChannelCloseInit
-	//MsgAcknowledgement     = ibcchannel.MsgAcknowledgement
-	//MsgTimeout             = ibcchannel.MsgTimeout
-	//MsgTimeoutOnClose      = ibcchannel.MsgTimeoutOnClose
 
 	MsgCreateFeed = oracle.MsgCreateFeed
 	MsgEditFeed   = oracle.MsgEditFeed
